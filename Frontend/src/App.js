25f898047c2f4878c4f1124e482397dd33066738
Asad Shah <asadali.shah@systemsltd.com>
2020-09-06 12:17:09 +0500

    msal added with login redirect
    Signed-off-by: Asad Shah <asadali.shah@systemsltd.com>

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import AuthContextContainer from './modules/auth/contextContainer';
 
 import Routes from './routes';
-import Notification from "./shared/modules/notification";
-import ErrorBoundary from "./shared/modules/error/ErrorBoundry";
+import Notification from './shared/modules/notification';
+import ErrorBoundary from './shared/modules/error/ErrorBoundry';
+import authService from './modules/auth/services/auth.service';
 
 function App() {
+  useEffect(() => {
+    authService.fetchAccessToken();
+    // .then(response => {
+    //    console.log('token:', response);
+    // });
+    // const user = authService.getUser();
+    // console.log(user);
+  }, []);
+
   return (
     <Notification>
       <ErrorBoundary>
         <BrowserRouter>
           <AuthContextContainer>
-            <Routes/>
+            <Routes />
           </AuthContextContainer>
         </BrowserRouter>
       </ErrorBoundary>
