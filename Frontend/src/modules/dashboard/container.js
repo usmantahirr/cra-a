2f0e91fc4fedda1d0161a60c2e8a8fa0bd54b943
Marib Sultan <marib.sultan@systemsltd.com>
2020-09-08 19:18:31 +0500

    Form rendered inside content

--- Frontend/src/modules/dashboard/container.js
+++ Frontend/src/modules/dashboard/container.js
@@ -5,6 +5,22 @@ const Dashboard = () => {
   // TODO: add call to check if patient is new or is registered already
   // TODO: Add call to get schema
 
+  const layout = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+  };
+
+  const tailLayout = {
+    wrapperCol: {
+      offset: 8,
+      span: 16,
+    },
+  };
+
   // eslint-disable-next-line global-require
   const formSchema = require('../../../src/staticFormSchemaMock.json');
   const [pageState, setPageState] = React.useState({ prev: null, curr: 0, next: null });
@@ -33,7 +49,32 @@ const Dashboard = () => {
     }
   };
 
-  return <DashboardPage pageState={pageState} goForward={goForward} goBack={goBack} />;
+  const onFinish = (values, formIndex) => {
+    // return { values, formIndex };
+    console.log('Success:', values, formIndex);
+    // const temp = savedForms;
+    // temp[formIndex] = values;
+    // setSavedForms(temp);
+    // setFormStep(Math.min(formSchema.length - 1, formStep + 1));
+  };
+
+  const onFinishFailed = (errorInfo, formIndex) => {
+    // return { errorInfo, formIndex };
+    console.log('Failed:', errorInfo, formIndex);
+  };
+
+  return (
+    <DashboardPage
+      pageState={pageState}
+      goForward={goForward}
+      goBack={goBack}
+      formSchema={formSchema}
+      layout={layout}
+      tailLayout={tailLayout}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    />
+  );
 };
 
 export default Dashboard;
--- Frontend/src/shared/pages/Dashboard.js
+++ Frontend/src/shared/pages/Dashboard.js
@@ -1,24 +1,67 @@
 import React from 'react';
 // import React, { useContext } from 'react';
-import { Layout } from 'antd';
+import { Layout, Form } from 'antd';
 // import NotificationContext from '../modules/notification/context';
 // import ErrorContext from '../modules/error/context';
 import DashboardTemplate from '../templates/dashboardTemplate';
 import Header from '../molecules/header';
 import Footer from '../molecules/footer';
+import CustomFormItem from '../modules/customForm/customFormItem';
 
 const { Content } = Layout;
 
 const Dashboard = props => {
   // const notification = useContext(NotificationContext);
   // const errorContext = useContext(ErrorContext);
-  console.log('Page state', props);
-  const { goBack, goForward } = props;
+  const { goBack, goForward, mainFormSchema, pageState } = props;
+
+  const _renderFieldArray = (fieldArray, form) => {
+    return fieldArray.map(field => <CustomFormItem key={field.id} {...field} form={form}></CustomFormItem>);
+  };
+
+  const _renderSection = (section, form) => {
+    return (
+      <div key={section.id}>
+        <h3>{section.sectionTitle}</h3>
+        {_renderFieldArray(section.fieldArray, form)}
+      </div>
+    );
+  };
+
+  const _renderStepForm = (step, stepsCount, currStep, currIndex) => {
+    const [form] = Form.useForm();
+    const { name, formOrientation, initialValues } = step;
+    const { layout, onFinish, onFinishFailed, onValuesChange, onFieldsChange } = props;
+    return (
+      <Form
+        key={`${step.id}${name}`}
+        hidden={currIndex !== currStep}
+        {...layout}
+        name={name}
+        form={form}
+        layout={formOrientation}
+        initialValues={initialValues}
+        onFinish={result => onFinish(result, currStep)}
+        onFinishFailed={result => onFinishFailed(result, currStep)}
+        onValuesChange={onValuesChange}
+        onFieldsChange={onFieldsChange}
+      >
+        <h1>{step.stepTitle}</h1>
+        {step.sections && step.sections.map(section => _renderSection(section, form))}
+      </Form>
+    );
+  };
+
+  const _renderStepsBody = (formSchema, currStep) => {
+    return (
+      formSchema && formSchema.map((step, currIndex) => _renderStepForm(step, formSchema.length, currStep, currIndex))
+    );
+  };
 
   return (
     <DashboardTemplate>
-      <Header />
-      <Content>this is content</Content>
+      <Header pageState={pageState} formSchema={mainFormSchema} />
+      <Content>{_renderStepsBody(mainFormSchema, pageState.curr)}</Content>
       <Footer goBack={goBack} goForward={goForward} />
     </DashboardTemplate>
   );
