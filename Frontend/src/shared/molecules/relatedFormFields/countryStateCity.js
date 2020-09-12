2f05cd63fc3f86895618012b55c460ea16a9e944
Asad Shah <asadali.shah@systemsltd.com>
2020-09-12 14:40:29 +0500

    Code Refactored country, city and state readonly
    Signed-off-by: Asad Shah <asadali.shah@systemsltd.com>

--- Frontend/src/shared/molecules/relatedFormFields/countryStateCity.js
+++ Frontend/src/shared/molecules/relatedFormFields/countryStateCity.js
@@ -10,18 +10,21 @@ const CountryStateCity = props => {
   const [cities, setCities] = useState([]);
   const [showState, setShowState] = useState(false);
 
-  const fetchCoutries = async () => {
+  const fetchCountries = async () => {
     try {
       const { data } = await service.getCountries();
       if (data && data.length > 0) setCountries(data);
       else setCountries([]);
+      // form.setFieldsValue({
+      //   [country.name]: { value: '5f589189175c69424c936728' },
+      // });
     } catch (error) {
       setCountries([]);
     }
   };
 
   useEffect(() => {
-    fetchCoutries();
+    fetchCountries();
   }, []);
 
   const fetchStates = async id => {
@@ -44,7 +47,7 @@ const CountryStateCity = props => {
     }
   };
 
-  const onCountryChange = value => {
+  const onCountryChange = ({ value }) => {
     form.setFieldsValue({
       [state.name]: undefined,
       [city.name]: undefined,
@@ -65,7 +68,7 @@ const CountryStateCity = props => {
     }
   };
 
-  const onStateChange = value => {
+  const onStateChange = ({ value }) => {
     form.setFieldsValue({
       [city.name]: undefined,
     });
@@ -93,6 +96,7 @@ const CountryStateCity = props => {
           <CustomSelect
             showSearch
             allowClear
+            labelInValue
             placeholder={placeholder}
             optionFilterProp="children"
             onChange={onCountryChange}
@@ -123,6 +127,7 @@ const CountryStateCity = props => {
             <CustomSelect
               showSearch
               allowClear
+              labelInValue
               placeholder={placeholder}
               optionFilterProp="children"
               onChange={onStateChange}
@@ -164,6 +169,7 @@ const CountryStateCity = props => {
           <CustomSelect
             showSearch
             allowClear
+            labelInValue
             placeholder={placeholder}
             optionFilterProp="children"
             filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
