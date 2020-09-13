20544df5341656000eb5facc72463c2a1173afe4
Hira Majid <hira.majid@systemsltd.com>
2020-09-13 17:49:31 +0500

    Successful Modal

--- Frontend/src/shared/templates/AuthPageTemplate.js
+++ Frontend/src/shared/templates/AuthPageTemplate.js
@@ -9,13 +9,13 @@ const AuthPageTemplate = ({ children }) => {
   return (
     <div className={styles.authcontainer}>
       <Row justify="center" className={styles.row}>
-        <Col span={20} className={styles.mgrow}>
+        <Col xs={23} md={23} xl={20} className={styles.mgrow}>
           <Row className={styles.row}>
-            <Col span={13} className={styles.formcontainer}>
+            <Col xs={24} md={13} xl={13} className={styles.formcontainer}>
               <Image src="/assets/img/logo.png" className={styles.logobox} />
               {children}
             </Col>
-            <Col span={11} className={styles.salutationcontainer}>
+            <Col md={11} xl={11} className={styles.salutationcontainer}>
               <div className={styles.salutationholder}>
                 <h2 className={styles.subheading}>
                   Welcome To&nbsp;
