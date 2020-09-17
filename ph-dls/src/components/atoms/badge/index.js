bf712348072321bbf88834def96cc1b7c2b9f468
Usman Tahir <usman.tahir@systemsltd.com>
2020-09-17 18:56:51 +0500

    added more components

--- ph-dls/src/components/atoms/badge/index.js
+++ ph-dls/src/components/atoms/badge/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Badge as AntBadge } from 'antd';
+import { BadgeProps } from 'antd/lib/badge';
+import styles from '../button/style.module.scss';
 
-const Badge = ({ count, children }) => (
-  <AntBadge count={12}>{children}</AntBadge>
+const Badge = ({ count, className, children, ...props }) => (
+  <AntBadge
+    className={className ? `${styles.button} ${className}` : styles.button}
+    count={count}
+    {...props}
+  >
+    {children}
+  </AntBadge>
 );
+
+Badge.propTypes = {
+  ...BadgeProps
+};
 
 export default Badge;
